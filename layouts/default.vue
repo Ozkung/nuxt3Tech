a159244b1d5599bdc108deca90ce39e2b376beb4
DeadlyLemonate <pongpop.suw@gmail.com>
2024-04-03 18:00:01 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,4 @@
+<template>
+  Teste
+  <slot />
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>testts</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
